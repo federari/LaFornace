e40d8d08c501ba844ffc73b314554c1727b6775e
federari <ferrarigraziano81@gmail.com>
2024-07-25 10:51:38 +0200

    Update post.js

--- post.js
+++ post.js
@@ -1,38 +1,37 @@
 document.getElementById('uploadForm').addEventListener('submit', function(event) {
-    const password = prompt("inserisci Password")
-    if(password = "Password123"){
+    if(prompt("inserisci Password") = "Password123"){
         event.preventDefault();
 
-    const titolo = document.getElementById('titolo').value;
-    const descrizione = document.getElementById('descrizione').value;
-    const link = document.getElementById('link').value;
-    const immagine = document.getElementById('immagine').value;
+        const titolo = document.getElementById('titolo').value;
+        const descrizione = document.getElementById('descrizione').value;
+        const link = document.getElementById('link').value;
+        const immagine = document.getElementById('immagine').value;
 
-    const data = {
-        titolo: titolo,
-        descrizione: descrizione,
-        link: link,
-        immagine: immagine
-    };
+        const data = {
+            titolo: titolo,
+            descrizione: descrizione,
+            link: link,
+            immagine: immagine
+        };
 
-    fetch('https://serverlafornace.adaptable.app/upload', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Articolo caricato con successo!');
-        } else {
+        fetch('https://serverlafornace.adaptable.app/upload', {
+            method: 'POST',
+            headers: {
+             'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Articolo caricato con successo!');
+            } else {
+                alert('Errore nel caricamento dell\'articolo.');
+            }
+        })
+        .catch((error) => {
+            console.error('Errore:', error);
             alert('Errore nel caricamento dell\'articolo.');
-        }
-    })
-    .catch((error) => {
-        console.error('Errore:', error);
-        alert('Errore nel caricamento dell\'articolo.');
-    });
+        });
     }
 });
