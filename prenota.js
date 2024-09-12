73f0665caf4ea9ee06fd6f6cb4ab7475e3b644fa
federari <ferrarigraziano81@gmail.com>
2024-09-12 11:02:22 +0200

    d

--- prenota.js
+++ prenota.js
@@ -1,13 +1,13 @@
-var i = 0
+var n = 0
 var g
-var d
+var giorni = []
+const nome = ""
+const cellulare = 0
 
   // Aggiungi un event listener per ogni bottone
 function prenota(g) {
 
-  if(g < 10){
-    g = "" + 0 + g
-  }
+
   // Seleziona il div con la classe "prenota-bottom" all'interno del bottone cliccato
   const divToChange = document.getElementById(g);
 
@@ -16,17 +16,48 @@ function prenota(g) {
     // Cambia il colore di background del div selezionato
     if(style.backgroundColor == "rgb(214, 56, 56)"){
       divToChange.style.backgroundColor = 'rgb(57, 54, 196)'; // Cambia da rosso a blu
-      if(i == 0){
-        d = g
-        i = 1
-      }else{
-        d = "" + d + g
-      }
+        giorni[n] = g
+        n++
     }else{
       divToChange.style.backgroundColor = 'rgb(214, 56, 56)'; // Cambia da blu a rosso
-      d = "" + d - g
+      for( var i = 0; i < giorni.length; i++){ 
+        if ( giorni[i] === g) {
+          giorni.splice(i, 1); 
+        }
+     }
+     n--
     }
-
-
-    alert(d)
 };
+
+
+function invia(){
+
+  nome = prompt("Inserire un nome")
+  cellulare = prompt("inserire un numero di telefono")
+    const data = {
+      nome: nome,
+      cellulare: cellulare,
+      giorni: giorni,
+  };
+
+    fetch('https://serverlafornace.adaptable.app/prenota', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.success) {
+          alert('Prenotazione avvenuta con successo!');
+      } else {
+          alert('Errore nel caricamento della prenotazione: ' + data.error + "Chiama l'assistenza");
+      }
+  })
+  .catch((error) => {
+      console.error('Errore:', error);
+      alert("Errore nel caricamento della prenotazione, Chiama l'assistenza");
+  });
+};
+
