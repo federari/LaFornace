ec93f56a62562e7b0b18e5f9855585f53f5264ce
federari <ferrarigraziano81@gmail.com>
2024-10-19 08:48:57 +0200

    s

--- post.js
+++ post.js
@@ -3,28 +3,49 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
 
     const titolo = document.getElementById('titolo').value;
     const descrizione = document.getElementById('descrizione').value;
-    const link = document.getElementById('link').value;
-    const immagine = document.getElementById('immagine').value;
     const password = document.getElementById('password').value;
-    const giornata = new Date()
-    const giornataformatt = giornata.getFullYear()*1000000 + giornata.getMonth()*10000 + giornata.getDate()*100 + giornata.getHours()
-    console.log(giornataformatt)
+    const giornata = new Date();
+    const giornataformatt = giornata.getFullYear()*1000000 + giornata.getMonth()*10000 + giornata.getDate()*100 + giornata.getHours();
+    
+    const fileInput = document.getElementById('immagine');
+    const file = fileInput.files[0];
+    
+    if (!file) {
+        alert('Per favore seleziona un\'immagine');
+        return;
+    }
 
-    const data = {
-        titolo: titolo,
-        descrizione: descrizione,
-        link: link,
-        immagine: immagine,
-        giornata: giornataformatt,
-        password: password
-    };
+    // Crea un form data per caricare l'immagine
+    const formData = new FormData();
+    formData.append('immagine', file);
 
-    fetch('https://serverlafornace.adaptable.app/upload', {
+    // Prima carica l'immagine al server
+    fetch('https://serverlafornace.adaptable.app/uploadImage', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.imageUrl) {
+            const articoloData = {
+                titolo: titolo,
+                descrizione: descrizione,
+                immagine: data.imageUrl, // Usa l'URL dell'immagine caricata
+                giornata: giornataformatt,
+                password: password
+            };
+
+            // Invia i dati dell'articolo insieme all'URL dell'immagine
+            return fetch('https://serverlafornace.adaptable.app/upload', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(articoloData)
+            });
+        } else {
+            throw new Error('Errore durante il caricamento dell\'immagine');
+        }
     })
     .then(response => response.json())
     .then(data => {
